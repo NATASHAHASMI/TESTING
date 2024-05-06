994c3b053e68e15a3f4d294bfe5aaeabc95c613f
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-05-06 15:36:09 +0530

    Update callbacks.py

--- ChannelBot/callbacks.py
+++ ChannelBot/callbacks.py
@@ -21,11 +21,10 @@ from ChannelBot.string_to_buttons import string_to_buttons
 
 
 # Callbacks
-@Client.on_callback_query()
+Client.on_callback_query()
 async def _callbacks(bot: Client, callback_query: CallbackQuery):
     user = await bot.get_me()
-    user_id = callback_query.from_user.id
-    mention = user["mention"]
+    mention = user.username if user.username else ''  # Use username if available, otherwise empty string
     query = callback_query.data.lower()
     if query.startswith("home"):
         if query == 'home':
